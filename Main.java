fdb1f9fab8ce144c42254ac769fd67937131e064
g070817 <baitiandeqiji@163.com>
2018-12-11 00:59:43 +0800

    add

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Starting...");
-        Test test = new Test("abcdddddxd");
+        Test test = new Test("abcddddvdxd");
         test.Test2('b');
         test.Test2('x');
     }
