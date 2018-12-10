f5ade683af24960164e39aa65b5474454dd2b410
g070817 <baitiandeqiji@163.com>
2018-12-11 01:56:56 +0800

    add

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Starting...");
-        Test test = new Test("abcddd dvdxd");
+        Test test = new Test("abcd dd dvdxd");
         test.Test2('b');
         test.Test2('x');
     }
